d8d9d765fccdb2d00c7e814b0977e4bdbe42ce07
Maja Scherman <maja.scherman@gmail.com>
2016-04-11 13:35:12 +0200

    Task 1 REALLY complete.

---
+++ src/task2/MainSimulation.java
@@ -0,0 +1,28 @@
+package task2;
+
+import java.util.*;
+import java.io.*;
+
+public class MainSimulation extends GlobalSimulation {
+
+	public static void main(String[] args) throws IOException {
+		Event actEvent;
+		State actState = new State(); // The state that should be used
+		// Some events must be put in the event list at the beginning
+		insertEvent(ARRIVAL, 0);
+		insertEvent(MEASURE, 5);
+
+		// The main simulation loop
+		while (actState.noMeasurements < 1000) {
+			actEvent = eventList.fetchEvent();
+			time = actEvent.eventTime;
+			actState.treatEvent(actEvent);
+		}
+
+		// Printing the result of the simulation, in this case a mean value
+		System.out.println("Average number of clients in queue 1: " + 1.0 * actState.accumulated1 / actState.noMeasurements);
+		System.out.println("Average number of clients in queue 2: " + 1.0 * actState.accumulated2 / actState.noMeasurements);
+		System.out.println("Probability of rejection: " + (1.0 * actState.rejected) / (1.0 * actState.nrOfArrivals));
+		System.out.println("Elapsed time: " + actState.time);
+	}
+}
