58cc1c1ed47652af7b8437677f2854fd2d5003f5
Maja Scherman <maja.scherman@gmail.com>
2016-04-11 10:06:36 +0200

    First solution for task 1, event scheduling, 2 queues, times exponentially distributed

--- src/task1/MainSimulation.java
+++ src/task1/MainSimulation.java
@@ -1,25 +1,28 @@
 package task1;
+
 import java.util.*;
 import java.io.*;
 
+public class MainSimulation extends GlobalSimulation {
 
-public class MainSimulation extends GlobalSimulation{
- 
-    public static void main(String[] args) throws IOException {
-    	Event actEvent;
-    	State actState = new State(); // The state that shoud be used
-    	// Some events must be put in the event list at the beginning
-        insertEvent(ARRIVAL, 0);  
-        insertEvent(MEASURE, 5);
-        
-        // The main simulation loop
-    	while (time < 5000){
-    		actEvent = eventList.fetchEvent();
-    		time = actEvent.eventTime;
-    		actState.treatEvent(actEvent);
-    	}
-    	
-    	// Printing the result of the simulation, in this case a mean value
-    	System.out.println(1.0*actState.accumulated/actState.noMeasurements);
-    }
+	public static void main(String[] args) throws IOException {
+		Event actEvent;
+		State actState = new State(); // The state that should be used
+		// Some events must be put in the event list at the beginning
+		insertEvent(ARRIVAL, 0);
+		insertEvent(MEASURE, 5);
+
+		// The main simulation loop
+		while (time < 10000) {
+			actEvent = eventList.fetchEvent();
+			time = actEvent.eventTime;
+			actState.treatEvent(actEvent);
+		}
+
+		// Printing the result of the simulation, in this case a mean value
+		System.out.println(1.0 * actState.accumulated1 / actState.noMeasurements);
+		System.out.println(1.0 * actState.accumulated2 / actState.noMeasurements);
+		System.out.println(
+				"Probability of rejection: " + (1 - (1.0 * actState.accumulated1) / (1.0 * actState.nrOfArrivals)));
+	}
 }
--- src/task1/State.java
+++ src/task1/State.java
@@ -1,53 +1,75 @@
 package task1;
+
 import java.util.*;
 import java.io.*;
 
-class State extends GlobalSimulation{
-	
-	// Here follows the state variables and other variables that might be needed
-	// e.g. for measurements
-	public int numberInQueue = 0, accumulated = 0, noMeasurements = 0;
+class State extends GlobalSimulation {
 
 	Random slump = new Random(); // This is just a random number generator
-	
-	
-	// The following method is called by the main program each time a new event has been fetched
-	// from the event list in the main loop. 
-	public void treatEvent(Event x){
-		switch (x.eventType){
-			case ARRIVAL:
-				arrival();
-				break;
-			case READY:
-				ready();
-				break;
-			case MEASURE:
-				measure();
-				break;
+
+	// Here follows the state variables and other variables that might be needed
+	// e.g. for measurements
+	public int nrOfArrivals = 0, numberInQueue1 = 0, numberInQueue2 = 0, accumulated1 = 0, accumulated2 = 0,
+			noMeasurements = 0;
+	public double beta1 = 2.1, beta2 = 5;
+	public double arrivalSpeed = 2, serviceTimeQ1 = (-beta1) * Math.log(1 - slump.nextDouble()), serviceTimeQ2 = 2,
+			measureTime = (-beta2) * Math.log(1 - slump.nextDouble());
+
+	// The following method is called by the main program each time a new event
+	// has been fetched
+	// from the event list in the main loop.
+	public void treatEvent(Event x) {
+		switch (x.eventType) {
+		case ARRIVAL:
+			arrival();
+			break;
+		case DEPARTURE_FROM_1:
+			departureFrom1();
+			break;
+		case DEPARTURE_FROM_2:
+			departureFrom2();
+			break;
+		case MEASURE:
+			measure();
+			break;
 		}
 	}
-	
-	
-	// The following methods defines what should be done when an event takes place. This could
-	// have been placed in the case in treatEvent, but often it is simpler to write a method if 
+
+	// The following methods defines what should be done when an event takes
+	// place. This could
+	// have been placed in the case in treatEvent, but often it is simpler to
+	// write a method if
 	// things are getting more complicated than this.
-	
-	private void arrival(){
-		if (numberInQueue == 0)
-			insertEvent(READY, time + 2*slump.nextDouble());
-		numberInQueue++;
-		insertEvent(ARRIVAL, time + 2.5*slump.nextDouble());
+
+	private void arrival() {
+		nrOfArrivals++;
+		if (numberInQueue1 < 10) {
+			if (numberInQueue1 == 0)
+				insertEvent(DEPARTURE_FROM_1, time + serviceTimeQ1);
+			numberInQueue1++;
+		}
+		insertEvent(ARRIVAL, time + arrivalSpeed);
 	}
-	
-	private void ready(){
-		numberInQueue--;
-		if (numberInQueue > 0)
-			insertEvent(READY, time + 2*slump.nextDouble());
+
+	private void departureFrom1() {
+		numberInQueue1--;
+		if (numberInQueue1 > 0)
+			insertEvent(DEPARTURE_FROM_1, time + serviceTimeQ1);
+		if (numberInQueue2 == 0)
+			insertEvent(DEPARTURE_FROM_2, time + serviceTimeQ2);
+		numberInQueue2++;
 	}
-	
-	private void measure(){
-		accumulated = accumulated + numberInQueue;
+
+	private void departureFrom2() {
+		numberInQueue2--;
+		if (numberInQueue2 > 0)
+			insertEvent(DEPARTURE_FROM_2, time + serviceTimeQ2);
+	}
+
+	private void measure() {
+		accumulated1 += numberInQueue1;
+		accumulated2 += numberInQueue2;
 		noMeasurements++;
-		insertEvent(MEASURE, time + slump.nextDouble()*10);
+		insertEvent(MEASURE, time + measureTime);
 	}
 }
