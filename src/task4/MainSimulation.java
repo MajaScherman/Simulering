3c2dee26f3c4a578db6dc5eb94843157aed3c5e5
Kristoffer Lundgren <kristoffer.lundgren@gmail.com>
2016-04-22 14:31:36 +0200

    .

--- src/task4/MainSimulation.java
+++ src/task4/MainSimulation.java
@@ -10,26 +10,49 @@ public class MainSimulation extends GlobalSimulation {
 		State actState = new State(); // The state that should be used
 		// Some events must be put in the event list at the beginning
 		insertEvent(ARRIVAL, 0);
-		insertEvent(MEASURE, actState.expDist(actState.beta2));
+		insertEvent(MEASURE, actState.T);
 
 		// The main simulation loop
-		while (actState.noMeasurements < 1000) {
+		while (actState.noMeasurements < actState.M) {
 			actEvent = eventList.fetchEvent();
 			time = actEvent.eventTime;
 			actState.treatEvent(actEvent);
 		}
-
-		// Printing the result of the simulation, in this case a mean value
-		System.out.println("Mean value of arrival speed: " + actState.arrivalSpeed);
-		System.out.println("Average number of clients in queue 1: " + 1.0 * actState.accumulated1 / actState.noMeasurements);
-		System.out.println("Average number of clients in queue 2: " + 1.0 * actState.accumulated2 / actState.noMeasurements);
-		System.out.println("Sum of average number of clients: " + 1.0 * (actState.accumulated1 + actState.accumulated2)/actState.noMeasurements);
 		
-		double sum = 0;
-		for (int i = 0; i < actState.meanTimeInQueue.size(); i++) {
-			sum += actState.meanTimeInQueue.get(i);
+		try {
+			ProcessBuilder process = new ProcessBuilder("/bin/rm", "/Users/krlun/git/Simulering/src/task4/a.m");
+			Process p = process.start();
+			try {
+				p.waitFor();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		System.out.println("Average time in queueing network: "+ sum/actState.meanTimeInQueue.size());
+		
+		try {
+			PrintWriter writer = new PrintWriter("/Users/krlun/git/Simulering/src/task4/a.m", "UTF-8");
+			StringBuilder outputLine = new StringBuilder();
+			outputLine.append("b = [");
+			writer.println(outputLine);
+			
+			for (int i = 0; i < actState.noMeasurements; i++) {
+				outputLine = new StringBuilder();
+				outputLine.append(actState.timeList.get(i) + " " + actState.numberBeingServicedList.get(i) + ";");
+				writer.println(outputLine);
+			}
+			outputLine = new StringBuilder();
+			outputLine.append("];");
+			writer.println(outputLine);
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// Printing the result of the simulation
+		System.out.println("Average number of clients being serviced: " + 1.0 * actState.accumulated / actState.noMeasurements);
+		System.out.println("Probability of rejection: " + (1.0 * actState.rejected) / (1.0 * actState.nrOfArrivals));
 
 	}
 }
--- src/task4/State.java
+++ src/task4/State.java
@@ -9,14 +9,11 @@ class State extends GlobalSimulation {
 
 	// Here follows the state variables and other variables that might be needed
 	// e.g. for measurements
-	public int nrOfArrivals = 0, numberInQueue1 = 0, numberInQueue2 = 0, accumulated1 = 0,
-			accumulated2 = 0, noMeasurements = 0;
-	public double beta1 = 1, beta2 = 5;
-	public double arrivalSpeed = 1.1, serviceTimeQ2 = 2;
+	public int nrOfArrivals = 0, numberBeingServiced = 0, accumulated = 0, noMeasurements = 0, rejected = 0;
+	public int n = 100, x = 10, M = 4000;
+	public double lambda = 4, T = 4;
 	public ArrayList<Double> timeList = new ArrayList<Double>();
-	public ArrayList<Integer> numberInQueue1List = new ArrayList<Integer>();
-	public ArrayList<Integer> numberInQueue2List = new ArrayList<Integer>();
-	public LinkedList<Double> arrivalTime = new LinkedList<Double>();
+	public ArrayList<Integer> numberBeingServicedList = new ArrayList<Integer>();
 	public ArrayList<Double> meanTimeInQueue = new ArrayList<Double>();
 
 	// The following method is called by the main program each time a new event
@@ -29,9 +26,6 @@ class State extends GlobalSimulation {
 			break;
 		case DEPARTURE_FROM_1:
 			departureFrom1();
-			break;
-		case DEPARTURE_FROM_2:
-			departureFrom2();
 			break;
 		case MEASURE:
 			measure();
@@ -47,40 +41,28 @@ class State extends GlobalSimulation {
 
 	private void arrival() {
 		nrOfArrivals++;
-		arrivalTime.add(time);
-		if (numberInQueue1 == 0)
-			insertEvent(DEPARTURE_FROM_1, time + expDist(beta1));
-		numberInQueue1++;
-		insertEvent(ARRIVAL, time + expDist(arrivalSpeed));
+		if (numberBeingServiced < n) {
+			insertEvent(DEPARTURE_FROM_1, time + x);
+			numberBeingServiced++;
+		} else {
+			rejected++;
+		}
+		insertEvent(ARRIVAL, time + expDist(lambda));
 	}
 
 	private void departureFrom1() {
-		numberInQueue1--;
-		if (numberInQueue1 > 0)
-			insertEvent(DEPARTURE_FROM_1, time + expDist(beta1));
-		if (numberInQueue2 == 0)
-			insertEvent(DEPARTURE_FROM_2, time + expDist(beta1));
-		numberInQueue2++;
-	}
-
-	private void departureFrom2() {
-		numberInQueue2--;
-		meanTimeInQueue.add(time - arrivalTime.pop());
-		if (numberInQueue2 > 0)
-			insertEvent(DEPARTURE_FROM_2, time + expDist(beta1));
+		numberBeingServiced--;
 	}
 
 	private void measure() {
-		accumulated1 += numberInQueue1;
-		accumulated2 += numberInQueue2;
+		accumulated += numberBeingServiced;
 		noMeasurements++;
-		insertEvent(MEASURE, time + expDist(beta2));
+		insertEvent(MEASURE, time + T);
 		timeList.add(time);
-		numberInQueue1List.add(numberInQueue1);
-		numberInQueue2List.add(numberInQueue2);
+		numberBeingServicedList.add(numberBeingServiced);
 	}
 
-	public double expDist(double beta) {
-		return (-beta) * Math.log(1 - slump.nextDouble());
+	public double expDist(double lambda) {
+		return (-1/lambda) * Math.log(1 - slump.nextDouble());
 	}
 }
